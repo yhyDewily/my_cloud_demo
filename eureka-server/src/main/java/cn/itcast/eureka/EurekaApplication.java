4f15583c139a04e508ff1971d2821bb40d5e5e92
yhyDewily <1016886553@qq.com>
2021-12-28 19:50:58 +0800

    Spring cloud with eureka

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Dewily
+ * @date 2021-12-28 14:10
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+
+}
